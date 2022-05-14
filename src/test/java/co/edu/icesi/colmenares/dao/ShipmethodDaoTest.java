65ae770b70962ae9528b7e171cc5be1d724fcf7c
Nicolas Colmenares <47890683+NickEsColR@users.noreply.github.com>
2022-05-14 18:33:37 -0500

    shipmethodDao test done

--- src/test/java/co/edu/icesi/colmenares/dao/ShipmethodDaoTest.java
+++ src/test/java/co/edu/icesi/colmenares/dao/ShipmethodDaoTest.java
@@ -3,16 +3,42 @@ package co.edu.icesi.colmenares.dao;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import co.edu.icesi.colmenares.Taller2.Taller2Application;
+import co.edu.icesi.colmenares.model.prchasing.Shipmethod;
 
 @SpringBootTest(classes = Taller2Application.class)
 class ShipmethodDaoTest {
 
+	@Autowired
+	private IShipmethodDao shipmethodDao;
+	
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Shipmethod s = new Shipmethod();
+		s.setShipmethodid(1);
+		shipmethodDao.save(s);
+		assertEquals(s, shipmethodDao.findById(1));
 	}
 
+	@Test
+	void findAllTest() {
+		assertEquals(1, shipmethodDao.findAll().size());
+	}
+	
+	@Test
+	void updateTest() {
+		Shipmethod s = shipmethodDao.findById(1);
+		s.setName("update");
+		shipmethodDao.update(s);
+		assertEquals(s.getName(), shipmethodDao.findById(1).getName());
+	}
+	
+	@Test
+	void deleteTest() {
+		shipmethodDao.delete(shipmethodDao.findById(1));
+		assertNull(shipmethodDao.findById(1));
+	}
 }
